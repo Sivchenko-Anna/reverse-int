16e44cedb495e85019040240c394cf87dd4dc659
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2022-07-27 12:45:49 +1000

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  if (n > 0) {
+    let arr = n.toString().split('').reverse();
+    let str = arr.join('');
+    return +str;
+  } else if (n < 0) {
+    let num = n * (-1);
+    let arr = num.toString().split('').reverse();
+    let str = arr.join('');
+    return +str;
+  } else if (n === 0) {
+    return 0;
+  }
 }
